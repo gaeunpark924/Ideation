59a05cf2391e2780302b27bb3acc787d48425eb4
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2021-11-08 10:39:50 +0900

    randomMatching_outline
    randomMatching_outline 수정

--- Ideation/src/screens/IdeaMatching.js
+++ Ideation/src/screens/IdeaMatching.js
@@ -1,17 +1,95 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
-export default function IdeaMatching() {
+//IdeaList 아이디어 리스트 페이지
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, TouchableOpacity, Modal} from 'react-native';
+import {Button, Header, Card, Icon} from 'react-native-elements';
+import Record from './Record';
+// import Modal from 'react-native-modal';
+const CenterComponent = () => {
   return (
-    <View style={styles.container}>
-      <Text>IdeaMatching</Text>
+    <View>
+      <Text style={styles.CenterComponent}>아이디어 리스트</Text>
     </View>
   );
-}
+};
+const RightComponent = () => {
+  return (
+    <View>
+      <Icon name="menu" />
+    </View>
+  );
+};
+const IdeaMatching = () => {
+  return (
+    <View style={styles.container}>
+      <Header
+        centerComponent={<CenterComponent />}
+        rightComponent={<RightComponent />}
+        placement="center"
+      />
+      <View>
+        <Button title="즉시기록 노출 아이디어 선택" />
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Text>아이디어 이름 </Text>
+        <Text>작성 시간</Text>
+      </View>
+      <View>
+        <View style={{flexDirection: 'row'}}>
+          <Card containerStyle={{margin: 2, width: 24}}>
+            <Card.Image source={require('../assets/pet1.jpg')}></Card.Image>
+          </Card>
+          <Card containerStyle={{margin: 2, width: 24}}>
+            <Card.Image source={require('../assets/pet1.jpg')}></Card.Image>
+          </Card>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Card containerStyle={{margin: 2, width: 24}}>
+            <Card.Image source={require('../assets/pet1.jpg')}></Card.Image>
+          </Card>
+          <Card containerStyle={{margin: 2, width: 24}}>
+            <Card.Image source={require('../assets/pet1.jpg')}></Card.Image>
+          </Card>
+        </View>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Text>아이디어 이름 </Text>
+        <Text>작성 시간</Text>
+      </View>
+      <View>
+        <View style={{flexDirection: 'row'}}>
+          <Card containerStyle={{margin: 2, width: 24}} />
+          <Card containerStyle={{margin: 2}} />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Card containerStyle={{margin: 2}} />
+          <Card containerStyle={{margin: 2}} />
+        </View>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Text>아이디어 이름 </Text>
+        <Text>작성 시간</Text>
+      </View>
+      <View>
+        <View style={{flexDirection: 'row'}}>
+          <Card containerStyle={{margin: 2, width: 24}} />
+          <Card containerStyle={{margin: 2}} />
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Card containerStyle={{margin: 2}} />
+          <Card containerStyle={{margin: 2}} />
+        </View>
+      </View>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  CenterComponent: {
+    fontSize: 24,
+  },
 });
+export default IdeaMatching;
