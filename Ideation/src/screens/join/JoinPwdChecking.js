9dc5459419ade881f7c8abb8113d8217267490df
gaeunpark924 <qkrrkdms924@gmail.com>
2022-01-15 01:17:28 +0900

    [#9] feat: 회원가입 Firebase Auth 연결, 이메일 인증 기능 추가

--- Ideation/src/screens/join/JoinPwdChecking.js
+++ Ideation/src/screens/join/JoinPwdChecking.js
@@ -1,15 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, TextInput, ToastAndroid, TouchableOpacity } from 'react-native';
 import {KeyboardAvoidingView} from 'react-native';
 import styles from '../../styles/style';
 import { Controller, useForm } from 'react-hook-form';
+import auth from '@react-native-firebase/auth';
 
-const showToast = () =>{
+const showClauseToast = () =>{
   ToastAndroid.show("이용약관 클릭", ToastAndroid.SHORT);
 };
+const showEmailAlreadyToast = () =>{
+  ToastAndroid.show("사용 중인 이메일 입니다", ToastAndroid.SHORT);
+}
+const showEmailInvalidToast = () => {
+  ToastAndroid.show("이메일 형식이 아닙니다")
+}
+
 const JoinPwdChecking = ({route, navigation}) => {
     const { emailValue, pwdValue } = route.params;
     const [ pwdCheckingValue, setPwdCheckingValue] = useState('');
+    const [ isLogged ,setIsLogged ] = useState(false);
+    
     const { control, watch, formState: { errors }, getValues} = useForm({
       mode : 'onChange', reValidateMode: 'onChange', criteriaMode: "firstError",  //mode : 'onChange' 값이 변할때마다 유효성 확인
       defaultValues:{
@@ -18,8 +28,35 @@ const JoinPwdChecking = ({route, navigation}) => {
     });
     const onPressNavigation = () =>{
       (errors.pwdCheckingForm === undefined && pwdCheckingValue !== undefined)
-      ? navigation.popToTop() //navigation.navigate("ideadevelop") // idea develop으로 변경
+      ? createUser()//navigation.popToTop() //navigation.navigate("ideadevelop") // idea develop으로 변경
       : null
+    }
+    async function createUser() {
+      console.log(emailValue, pwdValue);
+      await auth().createUserWithEmailAndPassword(emailValue,pwdValue)
+        .then((user) => {
+          checkEmailVerification()       
+        })
+        .catch((error) => {
+          if (error.code === 'auth/email-already-in-use'){
+            showEmailAlreadyToast()
+          } else if (error.code == 'auth/invalid-email'){
+            showEmailInvalidToast()
+          }
+        })
+    }
+    async function checkEmailVerification(){
+      if (auth().currentUser.emailVerified){
+        navigation.navigate("welcome")
+      } else {
+        await auth().currentUser.sendEmailVerification()
+          .then(()=>{
+            navigation.popToTop() //스택의 첫 화면으로 이동
+          })
+          .catch((error) => {
+            console.log('error',error)
+          })
+      } 
     }
     console.log(watch())
     //console.log('errors',errors.pwdCheckingForm) //에러 확인
@@ -34,7 +71,7 @@ const JoinPwdChecking = ({route, navigation}) => {
                   validate: value => value === pwdValue || '이전 비밀번호와 일치하지 않습니다.'
                 }}
                 render={(props) => 
-                  <TextInput
+                  <TextInput           //대문자로 시작하는건 React 컴포넌트//소문자는 html 컴포넌트
                     {...props}
                     underlineColorAndroid={'black'}
                     placeholder="비밀번호를 한번 더 확인해주세요."
@@ -62,7 +99,7 @@ const JoinPwdChecking = ({route, navigation}) => {
             <View style={{alignItems:'center',marginBottom:20}}>
               <Text
                 style={styles.textUseCondition}
-                onPress={() => showToast()}>
+                onPress={() => showClauseToast()}>
                   {"  이용악관 보기  "}
               </Text>
             </View>
@@ -74,6 +111,7 @@ const JoinPwdChecking = ({route, navigation}) => {
                 이용약관에 동의하고 가입완료하기
               </Text>
             </TouchableOpacity>
+            {/* { isLogged ? navigation.navigate("welcome"): <Text>초기화 중 입니다.</Text>} */}
           </View>
         </View>
     );
