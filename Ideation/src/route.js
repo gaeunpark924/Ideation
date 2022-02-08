0461eaa5be1918adcee63206d33bd14a5786a641
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-02-08 21:11:29 +0900

    #19 feat : header navigation style

--- Ideation/src/route.js
+++ Ideation/src/route.js
@@ -12,109 +12,148 @@ import welcome from './screens/welcome';
 import ideamatching from './screens/IdeaMatching';
 import idealist from './screens/IdeaList';
 import ideadevelop from './screens/IdeaDevelop';
-import ideadeveloptest from'./screens/IdeaDevelopTest';
+import ideadeveloptest from './screens/IdeaDevelopTest';
 
 // 앱이 각 화면이 전환될 수 있는 기본 틀을 제공한다.
 const Stack = createStackNavigator();
 
 const App = () => {
-  console.log("route")
+  console.log('route');
   return (
-    <Stack.Navigator initialRouteName='Loading' options={{headerStyle:{backgroundColor:'#fdf8ff'},}}>
-        {/* // screenOptions={{headerShown:false,headerBackTitleVisible: true, headerStyle: {backgroundColor: '#E7D9FF'},}}> */}
-      <Stack.Screen name="Loading" component={Loading} options={{headerShown:false}}/>
+    <Stack.Navigator
+      initialRouteName="Loading"
+      options={{headerStyle: {backgroundColor: '#fdf8ff'}}}>
+      {/* // screenOptions={{headerShown:false,headerBackTitleVisible: true, headerStyle: {backgroundColor: '#E7D9FF'},}}> */}
+      <Stack.Screen
+        name="Loading"
+        component={Loading}
+        options={{headerShown: false}}
+      />
       <Stack.Screen
         name="Login"
         component={Login}
         options={{
           title: '로그인',
-          headerStyle:{backgroundColor:'#fdf8ff',},
+          headerStyle: {backgroundColor: '#fdf8ff'},
           headerLeft: null,
-          headerTitleStyle:{
+          headerTitleStyle: {
             fontFamily: 'SB_Aggro_M',
-            fontSize: 28
-          }
-        }}/>
+            fontSize: 28,
+          },
+        }}
+      />
       <Stack.Screen
         name="LoginEmail"
         component={LoginEmail}
         options={{
           title: '이메일로 로그인',
-          headerStyle:{backgroundColor:'#fdf8ff',},
-          headerTitleStyle:{
+          headerStyle: {backgroundColor: '#fdf8ff'},
+          headerTitleStyle: {
             fontFamily: 'SB_Aggro_M',
-            fontSize: 28
-          }
-        }}/>
+            fontSize: 28,
+          },
+        }}
+      />
       <Stack.Screen
         name="JoinEmail"
         component={JoinEmail}
         options={{
           title: '회원가입 1/3단계',
-          headerStyle:{backgroundColor:'#fdf8ff',},
-          headerTitleStyle:{
+          headerStyle: {backgroundColor: '#fdf8ff'},
+          headerTitleStyle: {
             fontFamily: 'SB_Aggro_M',
-            fontSize: 28
-          }
-        }}/>
+            fontSize: 28,
+          },
+        }}
+      />
       <Stack.Screen
         name="JoinPwd"
         component={JoinPwd}
         options={{
           title: '회원가입 2/3단계',
-          headerStyle:{backgroundColor:'#fdf8ff',},
-          headerTitleStyle:{
+          headerStyle: {backgroundColor: '#fdf8ff'},
+          headerTitleStyle: {
             fontFamily: 'SB_Aggro_M',
-            fontSize: 28
-          }
-        }}/>
+            fontSize: 28,
+          },
+        }}
+      />
       <Stack.Screen
         name="JoinPwdChecking"
         component={JoinPwdChecking}
         options={{
           title: '회원가입까지 다 왔어요!',
-          headerStyle:{backgroundColor:'#fdf8ff',},
-          headerTitleStyle:{
+          headerStyle: {backgroundColor: '#fdf8ff'},
+          headerTitleStyle: {
             fontFamily: 'SB_Aggro_M',
-            fontSize: 28
-          }
-        }}/>
+            fontSize: 28,
+          },
+        }}
+      />
       <Stack.Screen
         name="SearchPwd"
         component={SearchPwd}
         options={{
           title: '비밀번호 찾기',
-          headerStyle:{backgroundColor:'#fdf8ff',},
-          headerTitleStyle:{
+          headerStyle: {backgroundColor: '#fdf8ff'},
+          headerTitleStyle: {
             fontFamily: 'SB_Aggro_M',
-            fontSize: 28
-          }
-        }}/>
-      <Stack.Screen name="welcome" component={welcome} options={{headerShown:false}}/>
-      <Stack.Screen name="idealist" component={idealist} options={{headerShown:false}} />
+            fontSize: 28,
+          },
+        }}
+      />
+      <Stack.Screen
+        name="welcome"
+        component={welcome}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="idealist"
+        component={idealist}
+        options={{headerShown: false}}
+      />
       <Stack.Screen
         name="ideadevelop"
         component={ideadevelop}
         options={{
-          headerStyle:{backgroundColor:'#fdf8ff',borderBottomWidth:2,borderBottomColor:'#1D1D1D',},
-          headerTitle: "Puzzle name",
-          headerTitleStyle:{
+          headerStyle: {
+            backgroundColor: '#fdf8ff',
+            borderBottomWidth: 2,
+            borderBottomColor: '#1D1D1D',
+          },
+          headerTitle: 'Puzzle name',
+          headerTitleStyle: {
             fontFamily: 'SB_Aggro_B',
-            fontSize: 20
-          }
-        }}/>
+            fontSize: 20,
+          },
+        }}
+      />
       <Stack.Screen
         name="ideadeveloptest"
         component={ideadeveloptest}
         options={{
-          headerStyle:{backgroundColor:'#fdf8ff',borderBottomWidth:2,borderBottomColor:'#1D1D1D',},
-          headerTitle: "Puzzle name",
-          headerTitleStyle:{
+          headerStyle: {
+            backgroundColor: '#fdf8ff',
+            borderBottomWidth: 2,
+            borderBottomColor: '#1D1D1D',
+          },
+          headerTitle: 'Puzzle name',
+          headerTitleStyle: {
             fontFamily: 'SB_Aggro_B',
-            fontSize: 20
-          }
-        }}/>
-      <Stack.Screen name="ideamatching" component={ideamatching} />
+            fontSize: 20,
+          },
+        }}
+      />
+      <Stack.Screen
+        name="ideamatching"
+        component={ideamatching}
+        options={{
+          headerShown: true,
+          headerBackTitleVisible: false,
+          title: '',
+          headerStyle: {backgroundColor: '#fdf8ff'},
+        }}
+      />
     </Stack.Navigator>
   );
 };
